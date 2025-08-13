0821cbe6a26f1c6cd94516e08aae1788949993ae
Sbillxx <146845774+Sbillxx@users.noreply.github.com>
2025-08-13 16:07:39 +0700

    tesssss port baru

--- twibbon-node/ecosystem.config.js
+++ twibbon-node/ecosystem.config.js
@@ -9,11 +9,13 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        PORT: 5000,
+        PORT: 5776,
+        HOST: "0.0.0.0",
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 5000,
+        PORT: 5776,
+        HOST: "0.0.0.0",
       },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
